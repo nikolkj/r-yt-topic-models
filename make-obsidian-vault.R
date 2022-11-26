10abe6c1709e4cc6144996d74c5da4cbc579fa7c
Nikolai Priser <nikolaipriser@gmail.com>
2022-11-26 13:14:35 -0600

    refactor topic pages

--- make-obsidian-vault.R
+++ make-obsidian-vault.R
@@ -2,21 +2,10 @@
 # pkgs 
 suppressPackageStartupMessages(require(tidyverse))
 suppressPackageStartupMessages(require(magrittr))
+suppressPackageStartupMessages(require(stm)) # required for `summary(topic_model)`
 
 # params
 param.k_select = 10
-
-# Make Obsidian Folders ----
-# make directories
-if(dir.exists("./obsidian-vault/")) unlink("./obsidian-vault/", recursive = TRUE) 
-dir.create("./obsidian-vault/")
-
-# make obsidian folders
-if(!dir.exists("./obsidian-vault/topics/")) dir.create("./obsidian-vault/topics/") # topics dir
-if(!dir.exists("./obsidian-vault/frex-stems/")) dir.create("./obsidian-vault/frex-stems/") # frex-stems dir
-if(!dir.exists("./obsidian-vault/lift-stems/")) dir.create("./obsidian-vault/lift-stems/") # frex-stems dir
-if(!dir.exists("./obsidian-vault/transcripts/")) dir.create("./obsidian-vault/transcripts/") # transcripts dir
-if(!dir.exists("./obsidian-vault/snippets/")) dir.create("./obsidian-vault/snippets/") # snippets dir
 
 # load data 
 load(file = "./prepd-data/topic-model-data.RData")
@@ -26,47 +15,78 @@ assertthat::assert_that(all(param.k_select %in% many_models$K))
 assertthat::assert_that(length(param.k_select) == 1L)
 topic_model = many_models$topic_model[[which(many_models$K == param.k_select)]]
 
-# Create Topic Folder Contents ----
-topic_model.summary = summary(topic_model)
-frex_words = as_tibble(topic_model.summary$frex, .name_repair = "unique") %>% 
-  mutate(K = row_number()) %>% 
-  pivot_longer(data = ., cols = -K, names_to = "dummy", values_to = "frex_word") %>% 
-  select(-dummy)
+# Make Obsidian Folders ----
+# make directories
+if(dir.exists("./obsidian-vault/")) unlink("./obsidian-vault/", recursive = TRUE) 
+dir.create("./obsidian-vault/")
 
-topic_numbers = sort(unique(frex_words$K))
-topic_labels = paste0("topic_", str_pad(string = topic_numbers, width = 2, side = "left", pad = "0"))
-for(i in seq_along(topic_numbers)){
-  .n = topic_numbers[i]
-  .lab = topic_labels[i] 
-  
-  .frex_words = filter(.data = frex_words, K == .n) %$% frex_word
-  .frex_words = paste0("[[", .frex_words, "]]")
-  .frex_words = c(.lab, # document title
-                  paste0("#", .lab), # document tag
-                  .frex_words # frex-words
-                  )
+# make main obsidian folders
+if(!dir.exists("./obsidian-vault/topics/")) dir.create("./obsidian-vault/topics/") # topics dir
+# if(!dir.exists("./obsidian-vault/frex-stems/")) dir.create("./obsidian-vault/frex-stems/") # frex-stems dir dynamic
+# if(!dir.exists("./obsidian-vault/lift-stems/")) dir.create("./obsidian-vault/lift-stems/") # frex-stems dir dynamic
+if(!dir.exists("./obsidian-vault/transcripts/")) dir.create("./obsidian-vault/transcripts/") # transcripts dir
+if(!dir.exists("./obsidian-vault/snippets/")) dir.create("./obsidian-vault/snippets/") # snippets dir
 
-  write_lines(x = .frex_words, 
-              file = paste0("./obsidian-vault/topics/",
-                            .lab, ".md"))
-  
-}
-
-# Make Main Topics Map ----
+# Map Document Transcript Topics ----
 theta = as_tibble(topic_model$theta)
 names(theta) = paste0("topic_",str_pad(seq(ncol(theta)), width = 2, side = "left", pad = "0"))
-theta$document = rownames(sparse_data)
-theta = select(.data = theta, document, everything())
+theta$ref = rownames(sparse_data)
+theta = select(.data = theta, ref, everything())
 
-main_topic = theta %>% 
+transcript_topics = theta %>% 
   pivot_longer(data = ., cols = contains("topic"), 
                names_to = "topic_id", values_to = "theta") %>% 
-  group_by(document) %>% 
+  group_by(ref) %>% 
   slice_max(.data = ., order_by = theta, n =  1) %>% 
   left_join(x = .,
             y = select(.data = titles, ref, title),
-            by = c("document" = "ref")) %>% 
-  mutate(topic_id = factor(topic_id))
+            by = "ref")
+
+topic_ids = sort(unique(transcript_topics$topic_id))
+assertthat::assert_that(any(!grepl('[*"\\/<>:|]', topic_ids))) # obsidian doc title char restricts
+
+# Create Topic Folder Contents ----
+# Each topic page contains: 
+#  - Title: `topic_id`
+#  - Topic Tag: "#topic_id" notation
+#  - Associated Token Links: FREX, LIFT, etc. tokens in "[[token]]" notation
+
+topic_model.summary = summary(topic_model)
+topic_model.tokens = list("frex" = as_tibble(topic_model.summary$frex, .name_repair = "unique"),
+                         "lift" = as_tibble(topic_model.summary$lift, .name_repair = "unique")) %>% 
+  bind_rows(., .id = "word_type") %>% 
+  group_by(word_type) %>% 
+  mutate(topic_id = row_number()) %>% 
+  ungroup() %>% 
+  mutate(topic_id = paste0("topic_", str_pad(topic_id, width = 2, side = "left", pad = "0"))) %>% 
+  pivot_longer(data = ., cols = -c(topic_id, word_type), names_to = "dummy_var", values_to = "token") %>% 
+  select(-dummy_var)
+  
+pb = progress::progress_bar$new(total = length(topic_ids))
+for(i in seq_along(topic_ids)){
+  .id = topic_ids[i]
+
+  .title = .id
+  .tag = paste0("#", .id)
+  
+  .tokens = topic_model.tokens %>% 
+    filter(topic_id == .id) %$%
+    token %>% 
+    paste0("[[", ., "]]")
+  
+  .path = paste0("./obsidian-vault/topics/", .id, ".md")
+  
+  .content = c(.title, 
+               .tag, 
+               .tokens
+               )
+  
+  write_lines(x = .content, 
+              file = .path)
+  
+  pb$tick()
+  
+}
 
 # Create Frex Stems Folder Contents ----
 stem_map = data %>% 
